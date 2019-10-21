f529f2fd4047a223c93f9579fa2be4470ce3fda6
Niels Swimberghe <nielsswimberghe@gmail.com>
2019-10-20 23:37:49 -0400

    Add project files.

---
+++ EFRawSQLSample/Migrations/20191021031503_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFRawSQLSample.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    PetId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.PetId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pets");
+        }
+    }
+}
---
+++ EFRawSQLSample/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EFRawSQLSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreatePets();
+            QueryPetsByType(type: "Cat");
+        }
+
+        private static void CreatePets()
+        {
+            using var petContext = new PetContext();
+            petContext.Database.EnsureCreated();
+            petContext.Database.ExecuteSqlRaw("DELETE FROM Pets WHERE 1 = 1");
+            petContext.Pets.Add(new Pet { Name = "Grumpy Cat", Type = "Cat" });
+            petContext.Pets.Add(new Pet { Name = "Smelly Cat", Type = "Cat" });
+            petContext.Pets.Add(new Pet { Name = "Lassie", Type = "Dog" });
+            petContext.SaveChanges();
+        }
+
+        private static void QueryPetsByType(string type)
+        {
+            using var petContext = new PetContext();
+            List<Pet> cats = petContext.Pets
+                .FromSqlRaw("SELECT [PetId], [Name], [Type] FROM Pets WHERE [Type] = {0}", type)
+                .ToList();
+
+            foreach (var cat in cats)
+            {
+                Console.WriteLine($"Name: {cat.Name}");
+            }
+        }
+    }
+
+    public class PetContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=pets.db");
+    }
+
+    public class Pet
+    {
+        public int PetId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
