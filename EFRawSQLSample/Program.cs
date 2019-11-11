14ba4dc40b1e4a5fe6d1c0923b50aa63404f9206
Niels Swimberghe <nielsswimberghe@gmail.com>
2019-11-11 15:08:39 -0500

    Add Keyless type to count pets by type

--- EFRawSQLSample/Program.cs
+++ EFRawSQLSample/Program.cs
@@ -11,6 +11,7 @@ namespace EFRawSQLSample
         {
             CreatePets();
             QueryPetsByType(type: "Cat");
+            QueryPetCountByType(type: "Cat");
         }
 
         private static void CreatePets()
@@ -27,23 +28,44 @@ namespace EFRawSQLSample
         private static void QueryPetsByType(string type)
         {
             using var petContext = new PetContext();
-            List<Pet> cats = petContext.Pets
+            List<Pet> pets = petContext.Pets
                 .FromSqlRaw("SELECT [PetId], [Name], [Type] FROM Pets WHERE [Type] = {0}", type)
                 .ToList();
 
-            foreach (var cat in cats)
+            foreach (var pet in pets)
             {
-                Console.WriteLine($"Name: {cat.Name}");
+                Console.WriteLine($"Name: {pet.Name}");
             }
+        }
+
+        private static void QueryPetCountByType(string type)
+        {
+            using var petContext = new PetContext();
+            PetCount petCount = petContext.PetCounts
+                .FromSqlRaw("SELECT COUNT(*) as 'Count' FROM Pets WHERE [Type] = {0}", type)
+                .Single();
+
+            Console.WriteLine($"Count: {petCount.Count}");
         }
     }
 
     public class PetContext : DbContext
     {
         public DbSet<Pet> Pets { get; set; }
+        public DbSet<PetCount> PetCounts { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite("Data Source=pets.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<PetCount>(eb =>
+                {
+                    eb.HasNoKey();
+                    eb.Property(Pet => Pet.Count).HasColumnName("Count");
+                });
+        }
     }
 
     public class Pet
@@ -51,5 +73,11 @@ namespace EFRawSQLSample
         public int PetId { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
+    }
+
+
+    public class PetCount
+    {
+        public int Count { get; set; }
     }
 }
